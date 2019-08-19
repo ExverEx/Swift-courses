b18c7ec1e9b25704ad23c1fa4328cc00cd3b0d6b
Alex <alex.exver@gmail.com>
2019-08-19 21:29:41 +0500

    дз к 4 уроку, пока только аватарка, кнопку еще не осилил

---
+++ LikeController.swift
@@ -0,0 +1,21 @@
+//
+//  LikeController.swift
+//  lesson4
+//
+//  Created by Alexander on 19/08/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class LikeController: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ lesson4/avatarView.swift
@@ -0,0 +1,18 @@
+//
+//  avatarView.swift
+//  lesson4
+//
+//  Created by Alexander on 18/08/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class avatarView: UIImageView {
+    
+    self.avatarView.layer.cornerRadius = self.avatarView.size.width / 2
+
+//    self.avatarView.cornerRadius = self.frame.size.width / 2
+//    self.clipsToBounds = true
+
+}
